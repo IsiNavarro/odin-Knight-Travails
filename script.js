25e182873d87600b65d36cd4e13393c9cb6faae1
Isidro Navarro Oporto <99091821+IsiNavarro@users.noreply.github.com>
2023-08-25 01:05:04 +0200

    Add KnightTravails() function. Not fully working

--- script.js
+++ script.js
@@ -1,49 +1,75 @@
+const q = [];
+
 class boardNode {
   constructor(index, neighbours = null) {
     this.index = index;
     this.neighbours = neighbours;
-    this.visited = null;
+    this.parent = null;
   }
-}
-function getNeighbours(root) {
-  const x = root.index[0];
-  const y = root.index[1];
+  getNeighbours() {
+    const x = this.index[0];
+    const y = this.index[1];
 
-  const possibleMoves = [
-    [-2, -1],
-    [-2, 1],
-    [-1, -2],
-    [-1, 2],
-    [1, -2],
-    [1, 2],
-    [2, -1],
-    [2, 1],
-  ];
+    const possibleMoves = [
+      [-2, -1],
+      [-2, 1],
+      [-1, -2],
+      [-1, 2],
+      [1, -2],
+      [1, 2],
+      [2, -1],
+      [2, 1],
+    ];
 
-  const neighbours = [];
+    const neighbours = [];
 
-  for (let i = 0; i < possibleMoves.length; i++) {
-    //Check X coordinate is in the board
-    //Check Y coordinate is in the board
-    if (
-      x + possibleMoves[i][0] < 0 ||
-      x + possibleMoves[i][0] > 7 ||
-      y + possibleMoves[i][1] < 0 ||
-      y + possibleMoves[i][1] > 7
-    ) {
-    } else {
-      const node = new boardNode([
-        x + possibleMoves[i][0],
-        y + possibleMoves[i][1],
-      ]);
-      neighbours.push(node);
+    for (let i = 0; i < possibleMoves.length; i++) {
+      //Check X coordinate is in the board
+      //Check Y coordinate is in the board
+      if (
+        x + possibleMoves[i][0] < 0 ||
+        x + possibleMoves[i][0] > 7 ||
+        y + possibleMoves[i][1] < 0 ||
+        y + possibleMoves[i][1] > 7
+      ) {
+      } else {
+        const node = new boardNode([
+          x + possibleMoves[i][0],
+          y + possibleMoves[i][1],
+        ]);
+        // Give each node its parent
+        node.parent = this;
+
+        neighbours.push(node);
+      }
     }
+    this.neighbours = neighbours;
   }
-  return neighbours;
 }
 
-const sampleNode = new boardNode([0, 0]);
-sampleNode.neighbours = getNeighbours(sampleNode);
-sampleNode.neighbours.forEach((neighbour) => {
-  console.log(neighbour.index);
-});
+console.log(KnightTravails([0, 0], [2, 4]));
+
+function KnightTravails(start, end) {
+  //Add the very first node
+  if (q.length === 0) q.push(new boardNode(start));
+  //1. Check if found
+  // Comparing arrays is tricky 'cause they're references
+  if (JSON.stringify(start) === JSON.stringify(end)) {
+    return q.shift();
+  } else {
+    //2. Shift queue
+    q.shift();
+
+    const startNode = new boardNode(start);
+    //3. Get neighbours
+    startNode.getNeighbours();
+
+    //4. Add neigbours to Queue
+    startNode.neighbours.forEach((neighbour) => {
+      q.push(neighbour);
+    });
+
+    //5 Pass first in line to the function back again
+    return KnightTravails(q[0].index, end);
+  }
+}
